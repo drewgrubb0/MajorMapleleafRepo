4fedd5cb35247d74386c194de00d180617bd7656
Brandon Alfred <bca25@txstate.edu>
2018-04-05 16:20:50 -0500

    customer model/controller updated

--- NUber/Customer/Customer.js
+++ NUber/Customer/Customer.js
@@ -1,1 +1,9 @@
-//File Placeholder
+var mongoose = require('mongoose');
+var CustomerSchema = new mongoose.Schema({
+    id: String,
+    name: String,
+    address: String
+});
+mongoose.model('Customer', CustomerSchema);
+
+module.exports = mongoose.model('Customer');
--- NUber/app.js
+++ NUber/app.js
@@ -2,4 +2,7 @@ var express = require('express');
 var app = express();
 var db = require('./db');
 
+var CustomerController = require('./Customer/CustomerController');
+app.use('/customer', CustomerController);
+
 module.exports = app;
