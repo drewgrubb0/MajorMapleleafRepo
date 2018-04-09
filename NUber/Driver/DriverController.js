f0f6038ecc11a5c07fed82641a887e36269bbf4d
CoronaClaudio <cjc262@txstate.edu>
2018-04-08 21:29:17 -0500

    Driver Update Availability
    Need to set condition that when setting to available, must give valid address for current address.

--- NUber/Driver/DriverController.js
+++ NUber/Driver/DriverController.js
@@ -7,5 +7,29 @@ router.use(bodyParser.json());
 var Driver = require('./Driver');
 
 //commands here
+// might not need this since admin makes it. Just adding it
+router.post('/', function (req, res) {
+    Driver.create({
+            id : req.body.id,
+            availability : req.body.availability,
+            name : req.body.name
+            currentCustomer : req.body.currentCustomer
+            currentAddress : req.body.currentAddress
+        }, 
+        function (err, user) {
+            if (err) return res.status(500).send("There was a problem adding the information to the database.");
+            res.status(200).send(user);
+        });
+});
+
+router.put('/:id', function (req, res) {
+    
+    Driver.findByIdAndUpdate(req.params.id, req.body, {new: true}, function (err, user) {
+        if (err) return res.status(500).send("You must either be set as available or unavailable.");
+        res.status(200).send(Driver);
+    });
+});
+
+
 
 module.exports = router;
